f9dddddd0bedc6006df2bef75908a77efc6ac84d
Sarn21150 <nsarn2009@gmail.com>
2025-07-17 22:14:37 +0700

    finally9

--- app/chatbot/page.tsx
+++ app/chatbot/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 export const dynamic = 'force-dynamic';
-
 import { useRouter, useSearchParams } from "next/navigation";
 import { useState, useRef, useEffect } from "react";
 import { GoogleGenerativeAI } from "@google/generative-ai";
