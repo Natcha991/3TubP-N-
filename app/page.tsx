ca787d3a9dec06fbf675dab79f7ee716d3ae405d
AdxiveDay <kaokongxddd@gmail.com>
2025-07-27 00:09:14 +0700

    เทสปุ่มกดส่ง

--- app/page.tsx
+++ app/page.tsx
@@ -77,7 +77,7 @@ export default function Home() {
     }
 
     return (
-        <div className="relative h-screen w-screen flex flex-col overflow-hidden items-center bg-gradient-to-br from-orange-300 to-orange-100">
+        <div className="relative h-[731px] w-screen flex flex-col overflow-hidden items-center bg-gradient-to-br from-orange-300 to-orange-100">
             <div className="absolute left-0">
                 <img src="/Group%2099.png" alt="Decoration"></img>
             </div>
