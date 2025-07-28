43eff52afedb6e024512f2c8016434609b6b4cae
AdxiveDay <kaokongxddd@gmail.com>
2025-07-28 22:27:23 +0700

    กหฟ

--- app/register3/register3ClientPage.tsx
+++ app/register3/register3ClientPage.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, useEffect, useCallback } from 'react';
+import { useState, useEffect } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
 
 export default function Register3() {
