e1bcb02c43a53ff2a43e10d388fc153a68a2f250
Natcha991 <Nutnutnut0911@gmail.com>
2025-10-14 21:51:42 +0700

    mongo

--- models/User.ts
+++ models/User.ts
@@ -3,7 +3,7 @@ import mongoose, { Schema } from 'mongoose';
 
 
 const UserSchema = new Schema({
-  name: { type: String, required: true, unique: true }, // ✅ ป้องกันชื่อซ้ำใน database
+  name: { type: String, required: true, unique: true },
   birthday: Date,
   gender: String,
   weight: Number,
@@ -11,6 +11,9 @@ const UserSchema = new Schema({
   goal: String,
   condition: String,
   lifestyle: String,
+  lineId: { type: String, unique: true, sparse: true },
+  awaitingName: { type: Boolean, default: true }, // ✅ เพิ่มบรรทัดนี้
 }, { timestamps: true });
+
 
 export default mongoose.models.User || mongoose.model('User', UserSchema, 'users');
